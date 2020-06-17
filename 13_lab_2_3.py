cf2dc922a8a11546f578f3ac4d327aace992524a
KornilovaEV <61602070+KornilovaEV@users.noreply.github.com>
2020-06-17 03:48:19 +0300

    Update 13_lab_2_3.py

--- 13_lab_2_3.py
+++ 13_lab_2_3.py
@@ -1,27 +1,46 @@
-def merge(left, right):
-    lst = []
-    while left and right:
-        if left[0] < right[0]:
-            lst.append(left.pop(0))
-        else:
-            lst.append(right.pop(0))
-    if left:
-        lst.extend(left)
-    if right:
-        lst.extend(right)
-    return lst
+import math as mth
+import sys
 
-def mergesort(lst):
-    length = len(lst)
-    if length >= 2:
-        сentre = int(length / 2)
-        lst = merge(mergesort(lst[:сentre]), mergesort(lst[сentre:]))
-    return lst
+def ProgBar(i, Range):
+    PER =  mth.floor(i/Range*100)
+    PRBAR = '#'*mth.floor(PER/2)+' '*mth.ceil((100-PER)/2)
+    sys.stdout.write('\rProgress: |{1}| [{0}%] '.format(PER, PRBAR))
+    sys.stdout.flush()
 
+def Sort(Arr):
+    FinArr = []
+    Range = len(Arr)
+    for i in range(len(Arr)):
+        FinArr.append(min(Arr))
+        Arr.remove(min(Arr))
+        if flag:
+            ProgBar(i, Range-1)
+    return FinArr
 
-try:
-    n = list(input())
-    print(mergesort(n))
-except ValueError:
-    print('Opssss, error')
+def MergeSort(InFile, OutFile):
+    global flag
+    file = open(InFile)
+    text = file.read().split("\n")
+    print("\nSorting the words in the lines."); flag = False
+    for i in range(len(text)):
+        text[i] = " ".join(Sort(text[i].split()))
+        ProgBar(i, len(text)-1)
+    print("\nSorting the lines in the text."); flag = True
+    text = "\n".join(Sort(text))
+    file = open(OutFile, "w")
+    file.write(text)
+    file.close()
+    print("\nDone!")
 
+if len(sys.argv) == 1:
+    names = input("Enter names of the input and" + \
+                  " output files using the spacebar: ").split()
+    InFile = names[0]; OutFile = names[1] 
+else:
+    if len(sys.argv) == 2:
+        names = open(sys.argv[1]).read().split("\n")
+        InFile = names[0]; OutFile = names[1]
+    else:
+        InFile = sys.argv[1]; OutFile = sys.argv[2]
+
+MergeSort(InFile, OutFile)
